aa552ae7f10ee2d9988cc294c4099f75cf43e85d
dcowen91 <dcowen@email.arizona.edu>
2018-06-23 11:34:47 -0700

    add basic cli arg handling

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,33 @@
 #!/usr/bin/env node
 
-const { paletteSearch } = require("./index");
-const fs = require("fs");
+const { paletteSearch } = require('./index');
+const fs = require('fs');
 
-const inputColor = process.argv[2];
-const inputFile = process.argv[3];
+// Validate input params
+if (process.argv.length != 4) {
+	printUsage();
+}
+// Validate input color
+else if (process.argv[2].length < 6 || process.argv[2].length > 7) {
+	printUsage();
+} else {
+	const inputColor = process.argv[2];
+	const inputFile = process.argv[3];
 
-const palette = JSON.parse(fs.readFileSync(inputFile));
+	try {
+		const palette = JSON.parse(fs.readFileSync(inputFile));
+		console.log(paletteSearch(inputColor, palette));
+	} catch (e) {
+		printUsage();
+	}
+}
 
-console.log(paletteSearch(inputColor, palette));
+function printUsage() {
+	const output = [
+		'Usage:',
+		'\tpalette-search <color> <theme.json>',
+		'\t<color>: a hex color i.e. #3B5C78',
+		"\t<theme.json>: a JSON file containing a list of named colors i.e. {'red': '#FF0000'}"
+	];
+	console.log(output.join('\n'));
+}
